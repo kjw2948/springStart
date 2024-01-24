1ecda4c7abf7d2af9b8f2eb49dae967e229f2ac3
kjw2948 <kjw970313@cau.ac.kr>
2024-01-24 15:34:33 +0900

    repository ex

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,23 @@
+package hello.hellospring.domain;
+
+public class Member {
+
+    private long id; //시스템이 저장하는 아이디
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
